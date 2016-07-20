881da8bb8dfdca58c9b1635ef766f54cb387fdd1
Kristoffe Biglete <kristoffe@falcon.io>
2016-07-20 15:54:45 +0200

    Default WP composer install

---
+++ vvv-init.sh
@@ -0,0 +1,25 @@
+
+if [ ! -d "frontend/wp-admin" ]; then
+	echo 'Installing WordPress (release version) in wp-vanilla/frontend...'
+	if [ ! -d "./frontend" ]; then
+		mkdir ./frontend
+	fi
+	cd ./frontend
+	wp core download --locale=en_US --allow-root
+	wp core config --dbname="wp-vanilla" --dbuser=wp --dbpass=wp --dbhost="localhost" --dbprefix=wp_ --locale=en_US --allow-root --extra-php <<PHP
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+define('SCRIPT_DEBUG', true);
+define('JETPACK_DEV_DEBUG', true);
+PHP
+	wp core install --url=wp-vanilla.dev --title="wp-vanilla" --admin_user=admin --admin_password=password --admin_email=admin@localhost.dev --allow-root
+	curl -s https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml > import.xml && wp plugin install wordpress-importer --allow-root  && wp plugin activate wordpress-importer --allow-root  && wp import import.xml --authors=skip --allow-root && rm import.xml
+
+  wp theme delete twentythirteen --allow-root; wp theme delete twentyfourteen --allow-root; wp theme delete twentyfifteen --allow-root; wp theme delete twentysixteen --allow-root; wp plugin delete hello --allow-root; wp plugin delete akismet --allow-root; git checkout HEAD .
+
+
+
+	cd -
+
+fi
---
+++ wp-config.php
@@ -0,0 +1,51 @@
+<?php
+
+
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wp-vanilla');
+
+/** MySQL database username */
+define('DB_USER', 'wp');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wp');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+define('AUTH_KEY',         'auE)K?afv2|D]LgdJDDTyj^k9+o KV90^oQJr,6y/pG-h3o++?07T+$w;WT8$I}Z');
+define('SECURE_AUTH_KEY',  'L.Pi,ME-J567l!}o[mvs1X.+-n+<<=z]*7QNL&3t-ew(No,^@G3eZvCbPZp9r=Y ');
+define('LOGGED_IN_KEY',    '*W(in8P~t(kMYOxvXsa8H }h4$b%|d*qbzsls|^GG!{9Eeqn_9M-1+Vc0MDmfTu1');
+define('NONCE_KEY',        'R7s4{p}cYS%c4k ?=4`TFvSqRdt!K&!2GeM)y?`4-6H&M`]C|b;eRD1m= n8R4A1');
+define('AUTH_SALT',        '|+`ld(%HL3}1b0-w_ljqtfm_PX.-$Lh=!jl09w[U?M:%(rLqtl|LR7}M(Edmkr=g');
+define('SECURE_AUTH_SALT', 'rUTZeD}QYy>/1xee4 O=IT#X7#--&aO(Nbc?}+zcre3C2UZ8:|0ETbKex/U=7J^b');
+define('LOGGED_IN_SALT',   'fGVUGi.7#jVYazwHJmBTbq(gKA2E}5JUo4]|~cNdCSO[+z`F]n|kzeMq,m2<;aa/');
+define('NONCE_SALT',       'Hs76_OXE9V:SqF{*;s~3G(j03_,zd;A_jZW`yZ|5XjVyd^KM,n-B0G|sOn^t+a=F');
+
+
+$table_prefix = 'wp_';
+
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_DEBUG_LOG', true);
+define('SCRIPT_DEBUG', true);
+define('JETPACK_DEV_DEBUG', true);
+
+
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
